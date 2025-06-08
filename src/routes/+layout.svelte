7b8c120bfe1b5e084a3d22ba32e69e969e133f48
fawlty-code <175266429+fawlty-code@users.noreply.github.com>
2025-06-09 04:50:50 +0530

    Initialize Svelte project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+
+	let { children } = $props();
+
+    const routes = ['collaborations', 'publications', 'research']; 
+</script>
+
+<nav class="flex justify-between mx-10 border-b-2 border-uninteresting-yellow">
+    <a href="/" class="p-4 text-2xl text-too-red font-anton font-bold">ICON</a>
+    <div class="flex">
+    {#each routes as route}
+        <a href="\{route}" class="p-4 text-xl text-funky-orange font-tt-hoves font-bold text-center">{route.charAt(0).toUpperCase() + route.slice(1)}</a>
+    {/each}
+	</div>
+</nav>
+
+{@render children()}
