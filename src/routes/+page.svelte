67437723e41f64b86b2352511c779facf603f96f
fawlty-code <175266429+fawlty-code@users.noreply.github.com>
2025-06-09 05:42:14 +0530

    Update homepage design

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<div class="grid grid-cols-3 grid-rows-3 p-2">
+    <div class="p-4 px-12 col-span-1 text-5xl text-too-red font-anton font-bold">ASHOKA BEHAVIOURAL INSIGHTS TEAM</div>
+    <div class="p-4 px-12 row-start-2 row-span-1 text-xl text-royal-blue font-tt-hoves font-extrabold">Striving to drive<br>impactful change</div>
+    <div class="p-4 px-12 col-span-2 row-span-3">
+        <p class="text-slate-gray font-helvetica font-semibold">The Ashoka Students Behavioural Insights Team (ABIT) is <b>India’s first student-led behavioural insights team</b>, operating out of Ashoka University, affiliated and funded by the <b>Centre for Social and Behaviour Change (CSBC)</b>. <br><br>We study all things B-Sci - policy, well-being, pop culture, consumer behaviour, organisational structures, business design and pretty much anything else under the sun. As a student-led organisation, we aim to explore and apply the many facets of behavioural science and interdisciplinary research to our every-day surroundings, and drive impactful change!</p>
+    </div>
+</div>
