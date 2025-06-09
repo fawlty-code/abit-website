5e9b4b631382829b8b108403f7879daa1275752d
fawlty-code <175266429+fawlty-code@users.noreply.github.com>
2025-06-09 18:07:36 +0530

    Add deco background image

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import deco from '$lib/assets/deco-bg.png';
+</script>
+
 <div class="grid grid-cols-3 p-2">
     <div class="p-4 pl-12 col-span-1 text-6xl text-too-red font-anton font-bold">ASHOKA BEHAVIOURAL INSIGHTS TEAM</div>
     <div class="relative pl-12 row-start-2">
@@ -6,4 +10,8 @@
     <div class="p-4 px-12 col-span-2 row-span-auto">
         <p class="text-slate-gray font-aileron font-semibold">The Ashoka Students Behavioural Insights Team (ABIT) is <b>India’s first student-led behavioural insights team</b>, operating out of Ashoka University, affiliated and funded by the <b>Centre for Social and Behaviour Change (CSBC)</b>. <br><br>We study all things B-Sci - policy, well-being, pop culture, consumer behaviour, organisational structures, business design and pretty much anything else under the sun. As a student-led organisation, we aim to explore and apply the many facets of behavioural science and interdisciplinary research to our every-day surroundings, and drive impactful change!</p>
     </div>
+</div>
+
+<div class="grid grid-cols-2">
+    <img src={deco} alt="80s deco background" class="col-start-1 row-start-1 col-span-2 px-24 pr-48">
 </div>
