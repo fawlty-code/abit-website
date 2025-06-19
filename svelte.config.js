1f1c3d0a5808f73c9a14c803c670ae4f777bdb50
fawlty-code <175266429+fawlty-code@users.noreply.github.com>
2025-06-19 05:50:20 +0530

    Change Adapter for Vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
-	},
-	paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
+	}
 };
 
 export default config;
